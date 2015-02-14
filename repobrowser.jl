84ab6cfb82c7d61733f73df2a8f5f012e2901284
David P. Sanders <dpsanders@gmail.com>
2015-02-14 01:45:10 -0600

    Refactored into browse_repo function, which accepts any directory and any filename in that git repo

--- repobrowser.jl
+++ repobrowser.jl
@@ -1,25 +1,38 @@
+# Only works from within IJulia, since uses widgets
 using Interact
 
-commits = reverse(readlines(`git log --oneline`))
-hashes = ASCIIString[split(commit)[1] for commit in commits];
-
-function use_commit(hash::String, filename::String)
-    run(`git show $hash:$filename` |> "_tmp.jl")
-end
-
-filename = "example_code/example.jl"
 
 # Concatenate two HTML representations
 # HTML is a type defined in Interact.jl
 +(a::HTML, b::HTML) = html(string(a.value, b.value))
 
-function generate_html(hash::String, filename::String)
-    use_commit(hash, filename)
+
+# Syntax for using git on another directory:
+# git -C ~/Dropbox/projects/RepoBrowser/ log --oneline
+
+# Get the copy of a filename from a particular commit with a given hash, in the given directory
+function use_commit(directory::String, hash::String, filename::String)
+    run(`git -C $directory show $hash:$filename` |> "_tmp.jl")  # pipe output to a temporary file
+end
+
+function generate_html(directory::String, hash::String, filename::String)
+    use_commit(directory, hash, filename)
     code = readall(`pygmentize -O full -f html -l julia _tmp.jl`);
     html(code)
 end
 
-@manipulate for n in 1:length(hashes)
-    html(commits[n]) + generate_html(hashes[n], filename)
+function browse_repo(directory::String, filename::String)
+    commits = reverse(readlines(`git -C $directory log --oneline`))
+    hashes = ASCIIString[split(commit)[1] for commit in commits];
+
+    @manipulate for n in 1:length(hashes)
+        html(commits[n]) + generate_html(directory, hashes[n], filename)
+    end
+
 
 end
+
+
+
+
+
