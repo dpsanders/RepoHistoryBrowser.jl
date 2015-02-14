25ad971904fd5cda234685707d8b1a8ba3891ae3
David P. Sanders <dpsanders@gmail.com>
2015-02-14 13:19:14 -0600

    Version using pygments

--- src/repobrowser.jl
+++ src/repobrowser.jl
@@ -5,7 +5,18 @@ module RepoBrowser
 export browse_repo
 
 using Interact
+using PyCall
 
+@pyimport pygments as pygments
+@pyimport pygments.lexers as lexers
+@pyimport pygments.formatters as formatters
+
+JuliaLexer = lexers.pymember(:JuliaLexer)
+HtmlFormatter = formatters.pymember(:HtmlFormatter)
+
+code = "f(x) = 3x"
+
+pygments.highlight(code, JuliaLexer(), HtmlFormatter(full=true))
 
 # Concatenate two HTML representations
 # HTML is a type defined in Interact.jl
@@ -16,14 +27,19 @@ using Interact
 # git -C ~/Dropbox/projects/RepoBrowser/ log --oneline
 
 # Get the copy of a filename from a particular commit with a given hash, in the given directory
-function use_commit(directory::String, hash::String, filename::String)
-    run(`git -C $directory show $hash:$filename` |> "_tmp.jl")  # pipe output to a temporary file
+function get_code_from_commit(directory::String, hash::String, filename::String)
+    # run(`git -C $directory show $hash:$filename` |> "_tmp.jl")  # pipe output to a temporary file
+    readall(`git -C $directory show $hash:$filename`)
 end
 
 function generate_html(directory::String, hash::String, filename::String)
-    use_commit(directory, hash, filename)
-    code = readall(`pygmentize -O full -f html -l julia _tmp.jl`);
-    html(code)
+    #use_commit(directory, hash, filename)
+    code = get_code_from_commit(directory, hash, filename)
+
+    # code = readall(`pygmentize -O full -f html -l julia _tmp.jl`);
+    highlighted_code = pygments.highlight(code, JuliaLexer(), HtmlFormatter(full=true))
+
+    html(highlighted_code)
 end
 
 function browse_repo(directory::String, filename::String)
